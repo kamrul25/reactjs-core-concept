7f9c45c6dd31eda251128d69d52eee0acda3ac74
kamrul25 <kamrul.coder01@gmail.com>
2023-03-24 19:29:19 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi!!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Kamrul" specialty="Web Developer"></Person>
+      <Person name="Shakib" specialty="Student"></Person>
+      <LoadPosts></LoadPosts>
     </div>
   );
+}
+
+
+function Person(props){
+  const [power, setPower] = useState(1);
+
+  const boostPower = () =>{
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+  return (
+    <dev>
+      <h2>Name:{props.name} </h2>
+      <p>Specialty: {props.specialty}  </p>
+      <p>Power: {power}</p>
+      <button onClick={boostPower}>Boost</button>
+    </dev>
+  )
+}
+
+function LoadPosts (){
+  const [posts, setPosts] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  }, []);
+
+  return (
+    <div >
+      {
+        posts.map(post=> <Post ha={post}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props){
+  return (
+    <div  className='displayStyle'>
+      <h2>Title: {props.ha.title} </h2>
+      <p>Body: {props.ha.body}</p>
+    </div>
+  )
 }
 
 export default App;
